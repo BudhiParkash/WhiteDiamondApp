4db1d6c5f528b3b91f61b448146b41cd8060b2df
Sahil <Lihaas@github.com>
2021-09-03 11:40:52 +0530

    home

--- app/src/main/java/com/example/whitediamond/MainActivity.java
+++ app/src/main/java/com/example/whitediamond/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.whitediamond;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private TextView mMenuHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+        mMenuHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void initView() {
+        mDrawerLayout = findViewById(R.id.drawerLayout);
+        mMenuHome = findViewById(R.id.menu_home);
     }
 }
